0da11ea521cac1fce88b1880a74290652d2eca1a
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-11-24 18:13:12 -0800

    Add button functionality

--- cipher.js
+++ cipher.js
@@ -5,3 +5,13 @@ function encrypt(plaintext, key) {
 function decrypt(ciphertext, key) {
 
 }
+
+$(document).ready(function() {
+  $("#encrypt").click(function() {
+    console.log("Encrypt pressed");
+  });
+
+  $("#decrypt").click(function() {
+    console.log("Decrypt pressed");
+  });
+});
