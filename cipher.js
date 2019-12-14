7f65a82cce737b9160aaf32ba704937b7a1d1cc3
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-12-13 17:50:43 -0800

    Implement cipher logic

--- cipher.js
+++ cipher.js
@@ -1,17 +1,56 @@
-function encrypt(plaintext, key) {
+function generateKey(text, key) {
+  newKey = key;
+  x = text.length;
+  for (i = 0; true; i++) {
+    if (i == x) {
+      i = 0;
+    }
+    if (newKey.length == text.length) {
+      break;
+    }
+    newKey += newKey.charAt(i);
+  }
+  return newKey;
+}
 
+function encrypt(plaintext, key) {
+  newKey = generateKey(plaintext, key);
+
+  ciphertext = "";
+  for (i = 0; i < plaintext.length; i++) {
+    p = plaintext.charCodeAt(i) - 97;
+    n = newKey.charCodeAt(i) - 97;
+
+    ciphertext += String.fromCharCode(((p + n) % 26) + 97)
+  }
+  return ciphertext;
 }
 
 function decrypt(ciphertext, key) {
+  newKey = generateKey(ciphertext, key);
 
+  plaintext = "";
+  for (i = 0; i < ciphertext.length; i++) {
+    c = ciphertext.charCodeAt(i) - 97;
+    n = newKey.charCodeAt(i) - 97;
+
+    plaintext += String.fromCharCode(((c - n + 26) % 26) + 97);
+  }
+  return plaintext;
 }
 
 $(document).ready(function() {
   $("#encrypt").click(function() {
-    console.log("Encrypt pressed");
+    plaintext = $("#plaintextInput").val();
+    key = $("#keyEncryptInput").val();
+    $("#output").text(encrypt(plaintext, key));
+    //console.log(encrypt(plaintext, key));
   });
 
   $("#decrypt").click(function() {
-    console.log("Decrypt pressed");
+    ciphertext = $("#ciphertextInput").val();
+    key = $("#keyDecryptInput").val();
+    $("#output").text(decrypt(ciphertext, key));
+    //console.log(decrypt(ciphertext, key));
   });
 });
